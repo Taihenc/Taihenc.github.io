c34bf23515dc3cdd81153d21945276acafca3859
Taihenc <51960471+Taihenc@users.noreply.github.com>
2023-12-09 21:29:33 +0700

    add text type animation add mockup animation

--- src/components/TypingEffect.tsx
+++ src/components/TypingEffect.tsx
@@ -4,10 +4,11 @@ import "../styles/cursor-style.css"
 interface TypingEffectProps {
   text: string[];
   endRemoveCursor?: boolean;
+  isTerminal?: boolean;
   onCharTypedEnd?: () => void;
 }
 
-const TypingEffect: React.FC<TypingEffectProps> = ({ text, endRemoveCursor, onCharTypedEnd }) => {
+const TypingEffect: React.FC<TypingEffectProps> = ({ text, endRemoveCursor, isTerminal, onCharTypedEnd }) => {
   const [typedText, setTypedText] = useState<string[]>([]);
   const codeRefs = Array.from({ length: text.length }, () => useRef<HTMLElement>(null));
   const [currentIndex, setCurrentIndex] = useState(0);
@@ -15,31 +16,32 @@ const TypingEffect: React.FC<TypingEffectProps> = ({ text, endRemoveCursor, onCh
   useEffect(() => {
     const intervalId = setTimeout(() => {
       if (currentIndex < text.length) {
+        codeRefs[currentIndex - 1]?.current?.classList.remove('cursor', 'terminal');
         const currentText = text[currentIndex];
         const currentTypedText = typedText[currentIndex] || '';
 
         if (currentTypedText.length < currentText.length) {
           const char = currentText.charAt(currentTypedText.length);
+          if (currentText === " ") {
+            setCurrentIndex((prev) => prev + 1);
+          }
           setTypedText((prev) => [
             ...prev.slice(0, currentIndex),
             currentTypedText + ((currentText === " ") ? '' : char),
             ...prev.slice(currentIndex + 1),
           ]);
         } else {
-          if (currentIndex < text.length - 1) {
-            codeRefs[currentIndex].current?.classList.remove('cursor');
-          }
           setCurrentIndex((prev) => prev + 1);
         }
       } else {
         if (endRemoveCursor) {
-          // Handle endRemoveCursor
+          codeRefs[currentIndex - 1]?.current?.classList.remove('cursor', 'terminal');
         }
         if (onCharTypedEnd) {
           onCharTypedEnd();
         }
       }
-    }, 100);
+    }, 20);
 
     return () => clearInterval(intervalId);
   }, [typedText, currentIndex]);
@@ -49,8 +51,8 @@ const TypingEffect: React.FC<TypingEffectProps> = ({ text, endRemoveCursor, onCh
       {typedText
         .filter((typed) => typed !== undefined)
         .map((typed, index) => (
-          <pre key={index} data-prefix={`${index + 1}`} className='w-full inline-flex whitespace-pre-wrap break-all'>
-            <code ref={codeRefs[index]} className='cursor'>
+          <pre key={index} data-prefix={(isTerminal) ? `$` : `${index + 1}`} className='w-full inline-flex whitespace-pre-wrap break-all'>
+            <code ref={codeRefs[index]} className={isTerminal ? 'terminal' : 'cursor'}>
               {typed}
             </code>
           </pre>
