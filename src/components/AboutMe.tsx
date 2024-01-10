c42fe1ac7842da316b2815e26a309eec7a3d7de7
Taihenc <51960471+Taihenc@users.noreply.github.com>
2024-01-11 01:55:38 +0700

    Update/Fix glitch AboutMe

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -7,11 +7,11 @@ export default function AboutMe() {
       <div id="about-me" className="w-full h-full bg-[#1a252f] bg-opacity-75">
         <div className="w-full h-full backdrop-blur-sm flex justify-center items-center overflow-hidden">
           <div className="w-full h-full md:p-28 p-6 flex justify-center items-center">
-            <div className="w-full h-full max-w-[100rem] max-h-[50rem] relative rounded-xl bg-opacity-[80%] bg-base-200">
+            <div className="w-full h-fit max-w-[100rem] max-h-[50rem] relative rounded-xl bg-opacity-[80%] bg-base-200">
               <div className="w-full h-full absolute -z-10 bg-transparent rounded-[inherit] shadow-2xl shadow-[rgba(252,225,7,0.4)] animate-pulse"></div>
               <div className="w-full h-full p-3 md:p-6 md:py-[5%] md:px-[7%] flex justify-center items-center">
-                <div className="w-full h-full flex flex-col lg:flex-row justify-center items-center">
-                  <div className="max-h-full w-[20rem] lg:mt-16 flex flex-col shrink-[2] justify-center items-center order-2">
+                <div className="w-full h-full max-h-[80vh] flex flex-col lg:flex-row justify-center items-center">
+                  <div className="max-h-full min-h-[10rem] w-[20rem] mb-5 lg:mt-16 flex flex-col shrink-[20] justify-center items-center order-3">
                     <div className="stack max-w-[18rem] lg:w-3/4 w-[40%] relative shrink-[2] rounded-lg shadow-2xl">
                       <div className="w-full h-full rounded bg-secondary text-secondary-content place-content-center overflow-hidden">
                         {/* <img src="https://dthezntil550i.cloudfront.net/sp/latest/sp2210222307207530022116305/1280_960/7c9ef945-f10b-4c00-beba-a53c8aa2a12b.png" className="w-full h-full bg-cover" /> */}
@@ -27,10 +27,11 @@ export default function AboutMe() {
                       <p className="pt-2 lg:pt-5 text-center text-xs md:text-sm text-yellow-300">Peerapat Patcharamontree</p>
                     </div>
                   </div>
+                  <div className="h-[5rem] max-h-[10rem] w-full lg:hidden shrink-[1] order-2"></div>
                   <div className="w-full max-w-[55rem] p-5 lg:pr-[7rem] md:pb-16 flex flex-col max-h-full shrink overflow-hidden order-1">
                     <h1 className="w-full mb-5 text-4xl lg:text-5xl font-bold text-red-200">Greeting!! 👋 I'm Atom</h1>
                     <h1 className="text-lg lg:text-2xl lg:pt-6 font-bold">About me</h1>
-                    <p className="max-h-[10rem] py-2 indent-6 overflow-auto shrink">
+                    <p className="md:max-h-[10rem] min-h-[5rem] py-2 indent-6 overflow-auto shrink">
                       A Shou-nen who is interested in Computer Programming. I have gone very wide in the Tech field,
                       but I consider my main areas of interest to be <span className="text-green-200">Web development, Android, Game development, and
                         Networking (home server)</span>. I also have a bit of a background in coding competitions. ... So far, this is all I know about
