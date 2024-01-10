d781ef04574bb03cb398f08f22a92bd3dcffe4e2
Taihenc <51960471+Taihenc@users.noreply.github.com>
2024-01-10 23:00:17 +0700

    fix a bit AboutMe glitch

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -11,8 +11,8 @@ export default function AboutMe() {
               <div className="w-full h-full absolute -z-10 bg-transparent rounded-[inherit] shadow-2xl shadow-[rgba(252,225,7,0.4)] animate-pulse"></div>
               <div className="w-full h-full p-3 md:p-6 md:py-[5%] md:px-[7%] flex justify-center items-center">
                 <div className="w-full h-full flex flex-col lg:flex-row justify-center items-center">
-                  <div className="max-h-full w-[20rem] md:mt-16 flex flex-col shrink justify-center items-center order-2">
-                    <div className="stack max-w-[18rem] md:w-3/4 w-[40%] relative shrink-[2] rounded-lg shadow-2xl">
+                  <div className="max-h-full w-[20rem] lg:mt-16 flex flex-col shrink-[2] justify-center items-center order-2">
+                    <div className="stack max-w-[18rem] lg:w-3/4 w-[40%] relative shrink-[2] rounded-lg shadow-2xl">
                       <div className="w-full h-full rounded bg-secondary text-secondary-content place-content-center overflow-hidden">
                         {/* <img src="https://dthezntil550i.cloudfront.net/sp/latest/sp2210222307207530022116305/1280_960/7c9ef945-f10b-4c00-beba-a53c8aa2a12b.png" className="w-full h-full bg-cover" /> */}
                         <img src="https://github.com/Taihenc.png" className="w-full h-full bg-cover" />
@@ -22,14 +22,14 @@ export default function AboutMe() {
                       <div className="w-full h-full rounded bg-[#223b93] text-primary-content place-content-center">1</div>
                       <div className="w-full h-full rounded bg-green-200 text-accent-content place-content-center">2</div>
                     </div>
-                    <div className="w-fit mt-2 md:mt-16 flex flex-col">
+                    <div className="w-fit mt-2 lg:mt-16 flex flex-col">
                       <p className="md:w-max text-sm md:text-base text-center font-serif">"If you gaze f or long into an abyss, nothing happens."</p>
-                      <p className="pt-2 md:pt-5 text-center text-xs md:text-sm text-yellow-300">Peerapat Patcharamontree</p>
+                      <p className="pt-2 lg:pt-5 text-center text-xs md:text-sm text-yellow-300">Peerapat Patcharamontree</p>
                     </div>
                   </div>
                   <div className="w-full max-w-[55rem] p-5 lg:pr-[7rem] md:pb-16 flex flex-col max-h-full shrink overflow-hidden order-1">
-                    <h1 className="w-full mb-5 text-4xl md:text-5xl font-bold text-red-200">Greeting!! 👋 I'm Atom</h1>
-                    <h1 className="text-lg md:text-2xl md:pt-6 font-bold">About me</h1>
+                    <h1 className="w-full mb-5 text-4xl lg:text-5xl font-bold text-red-200">Greeting!! 👋 I'm Atom</h1>
+                    <h1 className="text-lg lg:text-2xl lg:pt-6 font-bold">About me</h1>
                     <p className="max-h-[10rem] py-2 indent-6 overflow-auto shrink">
                       A Shou-nen who is interested in Computer Programming. I have gone very wide in the Tech field,
                       but I consider my main areas of interest to be <span className="text-green-200">Web development, Android, Game development, and
