aa1c25dd55869bd24fc48d1efc4bec228bc301b2
Taihenc <51960471+Taihenc@users.noreply.github.com>
2024-01-11 11:18:36 +0700

    fix reverse state Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -88,7 +88,7 @@ export default function NavBar() {
             <Disclosure.Button className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white">
               <span className="absolute -inset-0.5" />
               <span className="sr-only">Open main menu</span>
-              {!open ? (
+              {open ? (
                 <>
                   <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
                   <div className="absolute top-16 flex flex-col bg-[#1a252f] bg-opacity-50 rounded-lg gap-3">
