6733356614a7cb57daa180ee9f581d33756e698d
Taihenc <51960471+Taihenc@users.noreply.github.com>
2024-11-27 19:19:37 +0700

    update works exp titles

--- src/data/works-experience.ts
+++ src/data/works-experience.ts
@@ -3,7 +3,7 @@ import { WorkExperience } from '../interfaces/WorkExperience'
 export const workExperiences: WorkExperience[] = [
   {
     logo: './works-experience/lmwn/logo.png',
-    title: 'Lineman Wongnai internship🍾',
+    title: 'LMWN internship🍾',
     subtitle: "What I've learned^^",
     description:
       'I got an internship at Lineman Wongnai as a Software Engineer, specializing in Android development. I collaborated with the team to deliver new features more quickly, fix bugs, and migrate the old layout style to Jetpack Compose. I learned technologies such as Kotlin, Android development, and CI/CD pipelines. It was great to learn how a big company handles its large amount of traffic and data. Another thing to mention is that they treat intern students as full-time employees, so you get to experience the real work environment, which is great for seeing how your code actually works in the real world.',
@@ -32,7 +32,7 @@ export const workExperiences: WorkExperience[] = [
   },
   {
     logo: 'https://github.com/Taihenc/MoeKot/blob/main/app/src/main/ic_launcher-playstore.png?raw=true',
-    title: 'MoeKot(Android App)📱',
+    title: 'MoeKot📱',
     subtitle: "What's MoeKot?",
     description:
       'MoeKot is your go-to app for exploring, discovering, and curating your favorite manga and anime titles. With an intuitive interface and a touch of "Moe" (cute) inspiration, MoeKot enhances your browsing experience. Not only can you explore a vast collection of content, but future updates will also include a library feature for saving your favorite finds and tracking your reading/watching progress. The app is built using Kotlin and the MVVM architecture. Thanks to the API from AniList, the app provides the latest manga information. Although the app is not yet complete, you can check its progress on the GitHub repository.',
