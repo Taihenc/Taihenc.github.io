e018983954d320ca45baef98197d9e006520d211
Taihenc <51960471+Taihenc@users.noreply.github.com>
2024-11-27 10:06:27 +0700

    works section ver 1.2

--- src/components/Works.tsx
+++ src/components/Works.tsx
@@ -1,7 +1,4 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { scrollToSection } from "./Navbar"
-import TypingEffect from "./TypingEffect"
-import { useEffect, useState } from "react";
 import ImageCarousel from "./ImageCarousel";
 
 export default function Works() {
@@ -14,32 +11,27 @@ export default function Works() {
     // Add more images as needed
   ];
 
-  // State to track the current image index
-  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+  const handleContactClick = () => { }
 
-  // Effect to handle the auto-iteration of images
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setCurrentImageIndex((prevIndex: number) => (prevIndex + 1) % images.length); // Cycle through images
-    }, 3000); // Change image every 3 seconds
-
-    return () => clearInterval(interval); // Cleanup interval on component unmount
-  }, []);
+  const getLogoName = (src: string) => {
+    const name = src.split("/").pop();
+    return name?.split(".")[0] || "";
+  }
 
   return (
     <>
-      <div id="works" className="w-full h-full bg-[#1a252f] bg-opacity-75 snap-center p-6 pt-[4rem] md:pt-[6rem] md:px-[6rem] flex justify-center items-center overflow-hidden">
+      <section id="works" className="w-full h-full bg-[#1a252f] bg-opacity-75 snap-center overflow-hidden p-6 pt-[3rem] md:pt-[7rem] pb-[5rem] md:px-[7rem] flex justify-center items-center">
         <div className="w-full h-full gap-3 lg:gap-6 max-w-[100rem] flex flex-col lg:flex-row justify-center items-center">
-          <div className="h-fit max-w-full p-4 relative rounded-xl bg-opacity-[80%] bg-base-200">
-            <ul className="gap-5 flex flex-row lg:flex-col justify-start items-center overflow-y-auto">
-              {logos.map((src, index) => (
-                <li key={index} className="w-[3rem] lg:w-[5rem]  aspect-square rounded-md overflow-hidden flex-shrink-0 cursor-pointer">
-                  <img className="h-full w-full object-cover" src={src} alt={`Image ${index + 1}`} />
+          <aside className="h-fit md:h-full max-h-full max-w-full mb-auto p-4 relative rounded-xl bg-opacity-[80%] bg-base-200">
+            <ul className="h-full w-full gap-5 flex flex-row lg:flex-col justify-start items-center overflow-y-auto overflow-x-hidden">
+              {logos.map((src) => (
+                <li key={src} className="w-12 lg:w-20 aspect-square rounded-md overflow-hidden flex-shrink-0 cursor-pointer">
+                  <img className="h-full w-full object-cover" src={src} alt={`Logo of ${getLogoName(src)}`} loading="lazy" />
                 </li>
               ))}
             </ul>
-          </div>
-          <div className="h-full sm:max-h-[50rem] md:max-h-full w-full p-6 relative md:p-10 xl:py-[4rem] xl:px-[5rem] flex flex-col justify-between items-center bg-opacity-[80%] bg-base-200 rounded-xl">
+          </aside>
+          <article className="h-full sm:max-h-[50rem] md:max-h-full w-full p-6 relative md:p-10 xl:py-[4rem] xl:px-[5rem] flex flex-col justify-between items-center bg-opacity-[80%] bg-base-200 rounded-xl">
             <div className="w-full min-h-0 max-h-full flex flex-col flex-1 shrink overflow-hidden">
               <h1 className="w-full mb-5 text-4xl !leading-normal lg:text-5xl font-bold text-red-200">
                 LMWN INTERNSHIP
@@ -49,21 +41,24 @@ export default function Works() {
                   <ImageCarousel images={images} />
                 </div>
                 <div className="shrink-[1] min-w-0 w-full min-h-0 max-h-full flex flex-col">
-                  <h1 className="text-lg lg:text-2xl lg:pt-6 font-bold">Whats I've learn</h1>
-                  <p className="min-h-0 max-h-full py-2 indent-6 overflow-auto">
-                    Explanation about the works
-                    at <a className="link-primary" href="https://github.com/Taihenc" target="_blank">Github</a>.
+                  <h1 className="text-lg lg:text-2xl lg:pt-6 font-bold">What I've learned</h1>
+                  <p className="h-auto max-h-full py-2 indent-6 overflow-y-auto break-words">
+                    Explanation about the works at
+                    <a className="link-primary" href="https://github.com/Taihenc" target="_blank" rel="noopener noreferrer">Github</a>.
                   </p>
-                  <button onClick={() => scrollToSection('contact-me')} className="md:w-fit btn mt-3 text-white bg-[#223b93]">
+                  <button
+                    aria-label="Scroll to contact section"
+                    onClick={handleContactClick}
+                    className="md:w-fit btn mt-3 text-white bg-primary">
                     Contact Me
                     <FontAwesomeIcon icon={['fas', 'paper-plane']} />
                   </button>
                 </div>
               </div>
             </div>
-          </div>
+          </article>
         </div>
-      </div>
+      </section>
     </>
   )
 
